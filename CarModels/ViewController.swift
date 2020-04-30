fee944f394cced52f03444b3c0a0861c836a67c6
Alina <Ryabova>
2020-04-30 10:19:25 +0300

    Interface Outlets, Actions

--- CarModels/ViewController.swift
+++ CarModels/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+     
+     @IBOutlet weak var segmentedControl: UISegmentedControl!
+     @IBOutlet weak var markLabel: UILabel!
+     @IBOutlet weak var modelLabel: UILabel!
+     @IBOutlet weak var carImageView: UIImageView!
+     @IBOutlet weak var lastTimeStartedLabel: UILabel!
+     @IBOutlet weak var numberOfTripsLabel: UILabel!
+     @IBOutlet weak var ratingLabel: UILabel!
+     @IBOutlet weak var myChoiceImageView: UIImageView!
+     
+     @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
+         
+     }
+     
+     @IBAction func startEnginePressed(_ sender: UIButton) {
+         
+     }
+     
+     @IBAction func rateItPressed(_ sender: UIButton) {
+         
+     }
+     
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         // Do any additional setup after loading the view, typically from a nib.
+         
+     }
+     
 
 }
 
